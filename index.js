79ba2ed2531543e432525d16c4a72a6ae652e63f
codewithmokone <codewithmokone@gmail.com>
2023-10-16 11:55:35 +0200

    updated

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const express = require('express');
 const cors =require('cors');
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
 // const stripe = Stripe()
 
 const app = express();
